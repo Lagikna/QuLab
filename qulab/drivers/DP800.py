15a4bd483d22e72bd0b95e47dd1411a58c064815
Li Zhiyuan <32617128+Lagikna@users.noreply.github.com>
2019-01-26 17:58:56 +0800

    add PG_ADC and DP800 Drivers

---
+++ qulab/drivers/DP800.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+import numpy as np
+
+from qulab import BaseDriver, QInteger, QOption, QReal, QString, QVector
+
+
+class Driver(BaseDriver):
+    support_models = ['DP832']
+
+    quants = [
+        QReal('Voltage', value=0, unit='V', ch=1,
+          set_cmd=':SOUR%(ch)d:VOLT %(value).2f',
+          get_cmd=':SOUR%(ch)d:VOLT?'),
+
+         QOption('Output', ch=1,
+           set_cmd=':OUTP CH%(ch)d,%(option)s',
+           get_cmd=':OUTP? CH%(ch)d',
+           options=[('OFF', 'OFF'), ('ON', 'ON')]),
+    ]
