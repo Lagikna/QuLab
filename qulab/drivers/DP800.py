5473c9603ac4292cfeff2e4cbcd9bf563afec00d
Li Zhiyuan <32617128+Lagikna@users.noreply.github.com>
2019-01-28 15:57:41 +0800

    fix DP800

--- qulab/drivers/DP800.py
+++ qulab/drivers/DP800.py
@@ -5,10 +5,12 @@ from qulab import BaseDriver, QInteger, QOption, QReal, QString, QVector
 
 
 class Driver(BaseDriver):
+
     support_models = ['DP832']
+    '''Rigol DP800 series DC source'''
 
     quants = [
-        QReal('Voltage', value=0, unit='V', ch=1,
+        QReal('Offset', value=0, unit='V', ch=1,
           set_cmd=':SOUR%(ch)d:VOLT %(value).2f',
           get_cmd=':SOUR%(ch)d:VOLT?'),
 
